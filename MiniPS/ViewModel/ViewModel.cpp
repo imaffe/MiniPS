4a395ce2a433ced9ae53c0386b7da4f019653511
lewning <41135841+lewning@users.noreply.github.com>
2018-07-20 22:19:58 +0800

    open,save,contrast,brightness,filter*3

--- MiniPS/ViewModel/ViewModel.cpp
+++ MiniPS/ViewModel/ViewModel.cpp
@@ -1,22 +1,41 @@
 #include "ViewModel/ViewModel.h"
 #include "Model/Model.h"
 #include "Command//OpenFileCommand.h"
+#include "Command/ContrastCommand.h"
 #include "Notification/Notification.h"
+#include "Command/GaussianFilterCommand.h"
+#include "Command/Filter1Command.h"
+#include "Command/Filter2Command.h"
+#include "Command/SaveFileCommand.h"
 #include <QtGui/QImage>
 
-ViewModel::ViewModel() : qimg(std::shared_ptr<QImage>(new(QImage))) {
+ViewModel::ViewModel() : qimg_origin(std::shared_ptr<QImage>(new(QImage)))
+					, qimg_changed(std::shared_ptr<QImage>(new(QImage)))
+					, qimg_back(std::shared_ptr<QImage>(new(QImage)))
+					, cvimg_origin(std::shared_ptr<cv::Mat>(new (cv::Mat)))
+					, cvimg_changed(std::shared_ptr<cv::Mat>(new (cv::Mat)))
+					, cvimg_back(std::shared_ptr<cv::Mat>(new (cv::Mat)))
+{
     openFileCommand = std::static_pointer_cast<OpenFileCommand> \
         (std::shared_ptr<OpenFileCommand>(new OpenFileCommand(std::shared_ptr<ViewModel>(this))));
-    convertImageFormatSink = std::static_pointer_cast<ConvertImageFormatNotification>
-        (std::shared_ptr<ConvertImageFormatNotification>(new ConvertImageFormatNotification(std::shared_ptr<ViewModel>(this))));
+    contrastCommand = std::static_pointer_cast<ContrastCommand>\
+        (std::shared_ptr<ContrastCommand>(new ContrastCommand(std::shared_ptr<ViewModel>(this))));
+    gaussianFilterCommand= std::static_pointer_cast<GaussianFilterCommand> \
+        (std::shared_ptr<GaussianFilterCommand>(new GaussianFilterCommand(std::shared_ptr<ViewModel>(this))));
+    filter1Command= std::static_pointer_cast<Filter1Command> \
+        (std::shared_ptr<Filter1Command>(new Filter1Command(std::shared_ptr<ViewModel>(this))));
+    filter2Command = std::static_pointer_cast<Filter2Command> \
+        (std::shared_ptr<Filter2Command>(new Filter2Command(std::shared_ptr<ViewModel>(this))));
+    saveFileCommand= std::static_pointer_cast<SaveFileCommand> \
+        (std::shared_ptr<SaveFileCommand>(new SaveFileCommand(std::shared_ptr<ViewModel>(this))));
+
+    convertImageFormatSink = std::static_pointer_cast<ConvertImageFormatNotification>(std::shared_ptr<ConvertImageFormatNotification>(new ConvertImageFormatNotification(std::shared_ptr<ViewModel>(this))));
+
+
 }
 
 void ViewModel::bind(std::shared_ptr<Model> model) {
     this->model = model;
-}
-
-std::shared_ptr<ICommandBase> ViewModel::GetOpenFileCommand() {
-    return openFileCommand;
 }
 
 void ViewModel::SetUpdateViewNotifier(std::shared_ptr<IPropertyNotification> _updateViewNotifier) {
@@ -26,14 +45,13 @@ void ViewModel::SetUpdateViewNotifier(std::shared_ptr<IPropertyNotification> _up
 std::shared_ptr<IPropertyNotification> ViewModel::GetConvertImageFormatSink() {
     return convertImageFormatSink;
 }
-void ViewModel::ExecOpenFileCommand(std::string& path) {
-    model->OpenFile(path);
-}
+
 void ViewModel::ConvertImageFormat(){
-    (*qimg) = MatToQImage(*cvimg);
+    (*qimg_origin) = MatToQImage(*cvimg_origin);
+	(*qimg_back) = MatToQImage(*cvimg_back);
+	(*qimg_changed) = MatToQImage(*cvimg_changed);
     updateViewNotifier->OnPropertyChanged();
 }
-
 
 QImage ViewModel::MatToQImage(cv::Mat cvimg){
     cv::Mat tempimg;
@@ -43,13 +61,80 @@ QImage ViewModel::MatToQImage(cv::Mat cvimg){
                 static_cast<int>(tempimg.step), format).copy();
 }
 
-std::shared_ptr<QImage> ViewModel::GetQImage(){
-    return qimg;
+
+std::shared_ptr<QImage> ViewModel::GetQImage_origin()
+{
+	return qimg_origin;
+}
+void ViewModel::SetCvImage_origin(std::shared_ptr<cv::Mat> _cvimg)
+{
+	cvimg_origin = _cvimg;
+}
+std::shared_ptr<QImage> ViewModel::GetQImage_changed()
+{
+	return qimg_changed;
+}
+void ViewModel::SetCvImage_changed(std::shared_ptr<cv::Mat> _cvimg)
+{
+	cvimg_changed = _cvimg;
+}
+std::shared_ptr<QImage> ViewModel::GetQImage_back()
+{
+	return qimg_back;
+}
+void ViewModel::SetCvImage_back(std::shared_ptr<cv::Mat> _cvimg)
+{
+	cvimg_back = _cvimg;
 }
 
-void ViewModel::SetCvImage(std::shared_ptr<cv::Mat> _cvimg){
-    cvimg = _cvimg;
+
+
+void ViewModel::ExecOpenFileCommand(std::string& path) {
+	model->OpenFile(path);
+}
+void ViewModel::ExecContrastCommand(double contrast_value,double brightness_value)
+{   
+	model->Contrast(contrast_value,brightness_value);
+}
+void ViewModel::ExecGaussianFilterCommand()
+{
+	model->GaussianFilter(cv::Size(3,3),0, 0);
+}
+void ViewModel::ExecSaveFileCommand(std::string& path)
+{
+	model->SaveFile(path);
+}
+void ViewModel::ExecFilter1Command()
+{
+	model->Filter_1_Past();
+}
+void ViewModel::ExecFilter2Command()
+{
+	model->Filter_2_Black();
 }
 
+std::shared_ptr<ICommandBase> ViewModel::GetOpenFileCommand() {
+	return openFileCommand;
+}
+std::shared_ptr<ICommandBase> ViewModel::GetContrastCommand()
+{
+	return contrastCommand;
+}
+std::shared_ptr<ICommandBase> ViewModel::GetGaussianFilterCommand()
+{
+	return gaussianFilterCommand;
+}
+std::shared_ptr<ICommandBase> ViewModel::GetSaveFileCommand()
+{
+	return saveFileCommand;
+}
+std::shared_ptr<ICommandBase> ViewModel::GetFilter1Command()
+{
+	return filter1Command;
+}
+std::shared_ptr<ICommandBase> ViewModel::GetFilter2Command()
+{
+	return filter2Command;
+}
 
 
--- MiniPS/ViewModel/ViewModel.h
+++ MiniPS/ViewModel/ViewModel.h
@@ -11,22 +11,56 @@ class IPropertyNotification;
 class ViewModel
 {
 private:
-    std::shared_ptr<QImage> qimg;
-    std::shared_ptr<cv::Mat> cvimg;
+    std::shared_ptr<QImage> qimg_origin;
+    std::shared_ptr<cv::Mat> cvimg_origin;
+	std::shared_ptr<QImage> qimg_changed;
+	std::shared_ptr<cv::Mat> cvimg_changed;
+	std::shared_ptr<QImage> qimg_back;
+	std::shared_ptr<cv::Mat> cvimg_back;
+
     std::shared_ptr<Model> model;
 public:
-    std::shared_ptr<ICommandBase> openFileCommand;
+    
     std::shared_ptr<IPropertyNotification> convertImageFormatSink;
     std::shared_ptr<IPropertyNotification> updateViewNotifier;
+
+	std::shared_ptr<ICommandBase> openFileCommand;
+	std::shared_ptr<ICommandBase> contrastCommand;
+	std::shared_ptr<ICommandBase> gaussianFilterCommand;
+	std::shared_ptr<ICommandBase> brightCommand;
+	std::shared_ptr<ICommandBase> saveFileCommand;
+	std::shared_ptr<ICommandBase> filter1Command;
+	std::shared_ptr<ICommandBase> filter2Command;
 public:
     ViewModel();
     void bind(std::shared_ptr<Model> model);
-    std::shared_ptr<ICommandBase> GetOpenFileCommand();
     void SetUpdateViewNotifier(std::shared_ptr<IPropertyNotification>);
     std::shared_ptr<IPropertyNotification> GetConvertImageFormatSink();
-    void ExecOpenFileCommand(std::string&);
+
     void ConvertImageFormat();
     QImage MatToQImage(cv::Mat);
-    std::shared_ptr<QImage> GetQImage();
-    void SetCvImage(std::shared_ptr<cv::Mat>);
+
+    std::shared_ptr<QImage> GetQImage_origin();
+    void SetCvImage_origin(std::shared_ptr<cv::Mat>);
+	std::shared_ptr<QImage> GetQImage_changed();
+	void SetCvImage_changed(std::shared_ptr<cv::Mat>);
+	std::shared_ptr<QImage> GetQImage_back();
+	void SetCvImage_back(std::shared_ptr<cv::Mat>);
+
+	void ExecOpenFileCommand(std::string&);
+	void ExecContrastCommand(double,double);
+	void ExecGaussianFilterCommand();
+	void ExecBrightCommand(double);
+	void ExecSaveFileCommand(std::string&);
+	void ExecFilter1Command();
+	void ExecFilter2Command();
+
+	std::shared_ptr<ICommandBase> GetOpenFileCommand();
+	std::shared_ptr<ICommandBase> GetContrastCommand();
+	std::shared_ptr<ICommandBase> GetGaussianFilterCommand();
+	std::shared_ptr<ICommandBase> GetBrightCommand();
+	std::shared_ptr<ICommandBase> GetSaveFileCommand();
+	std::shared_ptr<ICommandBase> GetFilter1Command();
+	std::shared_ptr<ICommandBase> GetFilter2Command();
+
 };
